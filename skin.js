00cf60bcdb5ba3c42548eb35ae25dc9bef6e6493
stationdrei <74349125+stationdrei@users.noreply.github.com>
2021-01-13 17:00:10 +0100

    op[

--- skin.js
+++ skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 6.1.10/18007
 // Filename: simplex_Merc1.ggsk
-// Generated 2021-01-13T15:53:54
+// Generated 2021-01-13T16:58:03
 
 function pano2vrSkin(player,base) {
 	player.addVariable('opt_hotspot_preview', 2, true);
